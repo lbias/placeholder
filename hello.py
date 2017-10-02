35c71b0656a8faf19b1f60d201544340b6207fbc
lbias <306508354@qq.com>
2017-10-02 17:56:50 +0800

    wsgi application

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.conf.urls import url
 from django.conf import settings
+from django.core.wsgi import get_wsgi_application
 
 import sys
 
@@ -21,6 +22,8 @@ def index(request):
 urlpatterns = (
     url(r'^$', index),
 )
+
+application = get_wsgi_application()
 
 if __name__ == '__main__':
     from django.core.management import execute_from_command_line
