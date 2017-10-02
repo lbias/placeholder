bba23639a981d238e3bc2863aac7704b92295a24
lbias <306508354@qq.com>
2017-10-02 17:42:06 +0800

    the settings

--- hello.py
+++ hello.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse
 from django.conf.urls import url
+from django.conf import settings
 
 def index(request):
     return HttpResponse('Hello World')
 
 urlpatterns = (
     url(r'^$', index),
+)
+
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY='thisisthesecretkey',
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
 )
