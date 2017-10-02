0127f67d583ac119da340be6a5d4719a2b3d09fc
lbias <306508354@qq.com>
2017-10-02 17:49:40 +0800

    hello.py runserver

--- hello.py
+++ hello.py
@@ -2,12 +2,7 @@ from django.http import HttpResponse
 from django.conf.urls import url
 from django.conf import settings
 
-def index(request):
-    return HttpResponse('Hello World')
-
-urlpatterns = (
-    url(r'^$', index),
-)
+import sys
 
 settings.configure(
     DEBUG=True,
@@ -19,3 +14,14 @@ settings.configure(
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
     ),
 )
+
+def index(request):
+    return HttpResponse('Hello World')
+
+urlpatterns = (
+    url(r'^$', index),
+)
+
+if __name__ == '__main__':
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
